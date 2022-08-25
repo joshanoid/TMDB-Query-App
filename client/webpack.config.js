8698637a103e726baa6094f8ac9ec51d5b75fa88
Jozsef Varga <joshanoid@gmail.com>
2022-08-25 19:19:15 +0200

    webpack config added for client app

---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+
+module.exports = {
+    entry: './client/src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        port: 8080,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/src/index.html'),
+        }),
+    ],
+}
