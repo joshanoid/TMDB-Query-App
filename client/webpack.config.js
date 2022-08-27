1978a1cbddaa0752b48e5f5386f47126198f7544
Jozsef Varga <joshanoid@gmail.com>
2022-08-27 22:34:56 +0200

    entry path fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: './client/src/index.tsx',
+    entry: path.join(__dirname, '/src/index.tsx'),
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js',
