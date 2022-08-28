2492989768d794b4ae0da47752c8066f9dfa5c01
Jozsef Varga <joshanoid@gmail.com>
2022-08-28 23:07:25 +0200

    App component became the main entry with providers

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react'
-import { Box, IconButton, Stack, TextField, Typography } from '@mui/material'
-import SearchIcon from '@mui/icons-material/Search'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import './App.css'
+import { TMDB } from './TMDB'
+
+const queryClient = new QueryClient()
 
 export const App = () => (
-    <Stack spacing={2}>
-        <Typography variant="h1" gutterBottom align="center">
-            TMDB Query App
-        </Typography>
-        <Box width="100%" padding="0 5vw">
-            <Stack direction="row" width="100%">
-                <TextField label="Search..." variant="outlined" fullWidth />
-                <IconButton aria-label="search">
-                    <SearchIcon />
-                </IconButton>
-            </Stack>
-        </Box>
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+        <TMDB />
+    </QueryClientProvider>
 )
