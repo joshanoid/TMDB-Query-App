534542ee62eac6fb95b5f7e66d2321ddb78a78a4
Jozsef Varga <joshanoid@gmail.com>
2022-08-29 19:47:16 +0200

    fetch genres in the main component and passing down to movies

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,30 +1,49 @@
 import * as React from 'react'
-import { Box, IconButton, Stack, TextField, Typography } from '@mui/material'
+import { Box, CircularProgress, IconButton, Paper, Stack, TextField, Typography } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
+import { useQuery } from '@tanstack/react-query'
 
-import { Test } from 'features/Test'
-
+import { Movies } from 'features/Movies'
+import { fetchGenres } from 'utils/api'
 import './index.css'
 
 export const Main = () => {
     const [searchTerm, setSearchTerm] = React.useState('')
     const searchFieldRef = React.useRef<HTMLInputElement>(null)
+    const { data } = useQuery(['genres'], fetchGenres)
 
     return (
-        <Stack spacing={2}>
+        <Stack spacing={2} padding="0 5vw" height="100%">
             <Typography variant="h1" gutterBottom align="center">
                 TMDB Query App
             </Typography>
-            <Box width="100%" padding="0 5vw">
+            <Box width="100%">
                 <Stack direction="row" width="100%">
                     <TextField label="Search..." variant="outlined" fullWidth inputRef={searchFieldRef} />
                     <IconButton aria-label="search" onClick={() => setSearchTerm(searchFieldRef.current?.value ?? '')}>
                         <SearchIcon />
-                        <Test />
                     </IconButton>
                 </Stack>
-                {searchTerm}
             </Box>
+            {data ? (
+                <Stack direction="row" spacing={4} width="100%" flexGrow={1}>
+                    <Paper elevation={3} sx={{ width: '50%', height: '100%' }}>
+                        <Typography variant="h2">Search results:</Typography>
+                        {searchTerm === '' ? (
+                            <Typography variant="body2">Please use the search field above</Typography>
+                        ) : (
+                            <Movies searchTerm={searchTerm} genres={data} />
+                        )}
+                    </Paper>
+                    <Paper elevation={3} sx={{ width: '50%', height: '100%' }}>
+                        wefwef
+                    </Paper>
+                </Stack>
+            ) : (
+                <Box sx={{ display: 'flex' }}>
+                    <CircularProgress /> Loading
+                </Box>
+            )}
         </Stack>
     )
 }
