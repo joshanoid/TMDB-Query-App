67f70d0c1306c71fd66802323588e7b2ca150897
Jozsef Varga <joshanoid@gmail.com>
2022-08-29 19:45:58 +0200

    added cors support for the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,17 @@
 import * as express from 'express'
+import * as cors from 'cors'
 
 import genres from './routes/genres'
 import search from './routes/search'
 
 const app = express()
+const allowedOrigins = ['http://localhost:8080']
 
+const options: cors.CorsOptions = {
+    origin: allowedOrigins,
+}
+
+app.use(cors(options))
 app.use(genres)
 app.use(search)
 
